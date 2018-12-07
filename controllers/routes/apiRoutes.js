5198537fd9cb58ddb7e9df0418e3a857005e50dd
Will H <wandrewhouston@gmail.com>
2018-12-06 23:34:41 -0500

    created routes and updated character constructors and player model

--- controllers/routes/apiRoutes.js
+++ controllers/routes/apiRoutes.js
@@ -1,24 +1,14 @@
-var db = require('../../models')
-
+let db = require("../../models")
 module.exports = function(app) {
-    // Get all examples
-    app.get('/api/examples', function(req, res) {
-        db.Example.findAll({}).then(function(dbExamples) {
-            res.json(dbExamples)
+    app.post("/api/createaccount", function(req, res) {
+        db.Player.create(req.body).then(function(data) {
+            res.json(data)
         })
     })
 
-    // Create a new example
-    app.post('/api/examples', function(req, res) {
-        db.Example.create(req.body).then(function(dbExample) {
-            res.json(dbExample)
-        })
-    })
-
-    // Delete an example by id
-    app.delete('/api/examples/:id', function(req, res) {
-        db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-            res.json(dbExample)
+    app.post("/api/login", function(req, res) {
+        db.Player.findOne({where:{email:req.body.email}}).then(function(data) {
+            res.json(data)
         })
     })
 }
