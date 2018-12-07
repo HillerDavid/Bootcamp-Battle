792eebccdbcbc5de2c1e43011ed8fb76f078085c
Will H <wandrewhouston@gmail.com>
2018-12-06 20:33:39 -0500

    created player and enemy object constructors

---
+++ controllers/enemyObj.js
@@ -0,0 +1,7 @@
+function Enemy(enemyName, attack, defense, hp, expValue){
+    this.enemyName = enemyName;
+    this.attack = attack;
+    this.defense = defense;
+    this.hp = hp;
+    this.expValue = expValue;
+}
