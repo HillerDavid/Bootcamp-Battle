c58581e80c7bf3fce58007a94033dbd98cea13b0
mikey_bee <michael.betances93@gmail.com>
2018-12-08 09:46:58 -0500

    fixed errors and moved scripts

--- public/js/game.js
+++ public/js/game.js
@@ -2,19 +2,8 @@ let socket = io.connect();
 
 $('#terminal-button').on('click', function(event){
     event.preventDefault()
-    console.log('I was clicked.')
+    // console.log('I was clicked.')
     let userCommand = $('#terminal-cmd').val()
     $('#terminal-cmd').val('')
-    playerAction(userCommand)
+    console.log(userCommand)
 })
-
-playerAction = (command) => {
-    let message = ('<p style="color: lime;">').text(command)
-    $('#terminal-prompts').append(message)
-    enemyAction()
-}
-
-enemyAction = () => {
-    let message = ('<p style="color: red;">').text('There is definitely a spoon.')
-    $('#terminal-prompts').append(message)
-}
