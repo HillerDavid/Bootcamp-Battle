3c1bf15f293f2766a03536329f11ca47c9b77d88
mikey_bee <michael.betances93@gmail.com>
2018-12-08 10:41:07 -0500

    updated naming on index

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 $('#login-btn').on('click', function() {
     event.preventDefault()
-    let email = $('#inputUserName').val()
-    let password = $('#inputPassword').val()
+    let email = $('#input-email').val()
+    let password = $('#input-password').val()
     let number = Math.floor(Math.random() * 1000000)
     $.post('/api/login', { email: email, password: password, number: number }).then((data) => {
         console.log('Test')
