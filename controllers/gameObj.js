2e023fe2705e69a6f5218f9b801310d60499482b
Ben Houston <BenjaminAHouston@gmail.com>
2018-12-08 11:02:58 -0500

    Fixed issues with everything

--- controllers/gameObj.js
+++ controllers/gameObj.js
@@ -5,16 +5,16 @@ let game = {
     enemies: {},
     methods: {
         addPlayer: function(tempKey, data) {
-            list.players[tempKey] = {}
-            list.players[tempKey] = new Player(data.id, data.player_name, data.attack,
+            game.players[tempKey] = {}
+            game.players[tempKey] = new Player(data.id, data.player_name, data.attack,
                 data.defense, data.hp, data.mp, data.currency, data.homework_completed,
                 data.exp, data.level)
         },
         associatePlayer: function(tempKey, socketId) {
-            let temp = list.players[tempKey]
-            delete list.players[tempKey]
-            list.players[socketId] = temp
-            console.log(list.players)
+            let temp = game.players[tempKey]
+            delete game.players[tempKey]
+            game.players[socketId] = temp
+            console.log(game.players)
         }
     }
 }
