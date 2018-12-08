0253b9752bbf73e1f35c13b9c2b631529cb90825
Will H <wandrewhouston@gmail.com>
2018-12-08 10:34:26 -0500

    updated api routes

--- controllers/routes/apiRoutes.js
+++ controllers/routes/apiRoutes.js
@@ -1,14 +1,23 @@
 let db = require("../../models")
 module.exports = function(app) {
     app.post("/api/createaccount", function(req, res) {
-        db.Player.create(req.body).then(function(data) {
-            res.json(data)
+        db.Player.findOne({where:{email:req.body.email}}).then(function(data) {
+            if (!data) {
+                db.Player.create(req.body).then(function(data) {
+                    res.json(data)
+                })
+            } else {
+                res.end()
+            }
         })
     })
 
     app.post("/api/login", function(req, res) {
         db.Player.findOne({where:{email:req.body.email}}).then(function(data) {
-            res.json(data)
+            if (data) {
+                return res.json('/game')
+            }
+            res.end()
         })
     })
 }
