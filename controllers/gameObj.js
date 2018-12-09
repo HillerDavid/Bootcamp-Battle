ee0487bfd4269b9daf3a8be87f58f9860dcd3164
Ben Houston <BenjaminAHouston@gmail.com>
2018-12-09 16:05:36 -0500

    Fixed an issue with how I was resetting players references, also commented code I forgot and made some code more readable

--- controllers/gameObj.js
+++ controllers/gameObj.js
@@ -10,24 +10,40 @@ let game = {
     
     //This is the list of game methods in object form for easy of use
     methods: {
+        //Add a player with a tempKey to the game player object
         addPlayer: function(tempKey, data) {
+            //Create the reference where the player will be kept
             game.players[tempKey] = {}
+            //Create a 'new' player from the data provided and store it in the reference spot
             game.players[tempKey] = new Player(data.id, data.player_name, data.attack,
                 data.defense, data.hp, data.mp, data.currency, data.homework_completed,
                 data.exp, data.level)
         },
+
+        //When a player connects on socket associate that socket id with the player object already created
         associatePlayer: function(tempKey, socketId) {
+            //Store the object in a temporary variable
             let temp = game.players[tempKey]
+            //Delete the reference to the player stored in the temporary spot
             delete game.players[tempKey]
+            //Store the object at it's new location: the player's soccket connection id
             game.players[socketId] = temp
+            //Set reference in the player object to the socket id in case it's needed
             game.players[socketId].reference = socketId
         },
+
+        //Create an enemy object in the game enemy object list and associate it with a player
         createEnemy: function(players) {
+            //Create the reference where the enemy will be kept
             game.enemies[players[0].reference] = {}
+            //Create the enemy object and put it in it's location, giving it an array of players it is fighting
             game.enemies[players[0].reference] = new Enemy('assignment', 1, 1, 2, 100, players[0].reference, players)
-            for(let i = 0; i < players.length; i++) {
-                if (!game.players[players[i].reference].currentEnemy) {
-                    game.players[players[i].reference].currentEnemy = game.enemies[players[0].reference]
+            //Loop through the players
+            for(let player of players) {
+                //If the player isn't currently fighting an enemy
+                if (!player.currentEnemy) {
+                    //Set the player's currentEnemy to this one
+                    player.currentEnemy = game.enemies[players[0].reference]
                 }
             }
         }
