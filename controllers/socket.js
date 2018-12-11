bd229057641b759c75e78fa4d889feb8c96011c9
Ben Houston <BenjaminAHouston@gmail.com>
2018-12-11 17:03:42 -0500

    Fixed issue with items not being loaded properly

--- controllers/socket.js
+++ controllers/socket.js
@@ -51,7 +51,8 @@ module.exports = function(io, game) {
                     } else if (game.players[socket.id].currentEnemy.canAttack()){
                         console.log('Enemy is alive and will attack')
                         //Set a timer so it will attack after 2 seconds
-                        setTimeout(enemyAttack, 2000)
+                        enemyAttack()
+                        // setTimeout(enemyAttack, 2000)
                     } else {
                         //Some players have not attacked yet, so it is still their turn
                         console.log(`Enemy is alive but can't attack yet`)
