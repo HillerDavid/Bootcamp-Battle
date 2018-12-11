8d55809e39c3462be9bbffeb4a5bf1d6db9ffa5a
mikey_bee <michael.betances93@gmail.com>
2018-12-11 00:06:07 -0500

    touch ups to slack - awaiting review

--- public/js/game.js
+++ public/js/game.js
@@ -8,3 +8,33 @@ $('#terminal-button').on('click', function (event) {
     socket.emit('command', userCommand)
     $('#terminal-cmd').val('')
 })
+
+$('#chat-button').on('click', function (event) {
+    event.preventDefault()
+    let message = $('#chat-message-area').val()
+    let chatDiv = $('<div>')
+    chatDiv.addClass('outgoing-message-container lighter')
+    let avatar = $('<img>')
+    avatar.attr({
+        'src': '/images/default-slack-1.png'
+    }, {
+        'alt': 'Avatar'
+    })
+    let messageText = $('<p>')
+    messageText.text(message)
+    chatDiv.append(avatar, messageText)
+    $("#chat").append(chatDiv)
+        .animate({
+            scrollTop: $('#chat').prop("scrollHeight")
+        }, 500);
+    $('#chat-message-area').val('')
+    scrollChat()
+})
+
+function scrollChat() {
+    let chatLength = $('#chat > .outgoing-message-container').length
+    // console.log(chatLength)
+    if (chatLength > 6) {
+        $('#chat > .outgoing-message-container:first-child').remove()
+    }
+}
