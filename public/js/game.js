a6f77f64a1d7ecff0b24f9b3fe52e2e58958c873
mikey_bee <michael.betances93@gmail.com>
2018-12-10 22:20:27 -0500

    slacker updates

--- public/js/game.js
+++ public/js/game.js
@@ -1,7 +1,7 @@
 let socket = io.connect();
 socket.emit('identifier', localStorage.number)
 
-$('#terminal-button').on('click', function(event){
+$('#terminal-button').on('click', function (event) {
     event.preventDefault()
     // console.log('I was clicked.')
     let userCommand = $('#terminal-cmd').val()
