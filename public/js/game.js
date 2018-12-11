a53a35f61feff8994a1147d7061c1b692598a74d
David Hiller <HillerDavid@users.noreply.github.com>
2018-12-10 22:33:06 -0500

    Terminal emulator now accepts command inputs

--- public/js/game.js
+++ public/js/game.js
@@ -1,20 +1,8 @@
 let socket = io.connect();
 socket.emit('identifier', localStorage.number)
 
-$('#terminal-button').on('click', function (event) {
-    event.preventDefault()
-    // console.log('I was clicked.')
-    let userCommand = $('#terminal-cmd').val()
+$('#terminal').terminal((command, term) => {
+    console.log(command)
+    let userCommand = command
     socket.emit('command', userCommand)
-    $('#terminal-cmd').val('')
-})
-
-
-$('#terminal').terminal(function (command) {
-    if (command !== '') {
-        var result = window.eval(command);
-        if (result != undefined) {
-            this.echo(String(result));
-        }
-    }
 })
