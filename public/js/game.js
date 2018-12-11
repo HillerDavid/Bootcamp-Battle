1c36ce03d83dc6d95be1819cb6cc238d84dd4eef
David Hiller <HillerDavid@users.noreply.github.com>
2018-12-10 22:07:49 -0500

    Terminal emulator properly appears on page with working scrolling

--- public/js/game.js
+++ public/js/game.js
@@ -1,10 +1,20 @@
 let socket = io.connect();
 socket.emit('identifier', localStorage.number)
 
-$('#terminal-button').on('click', function(event){
+$('#terminal-button').on('click', function (event) {
     event.preventDefault()
     // console.log('I was clicked.')
     let userCommand = $('#terminal-cmd').val()
     socket.emit('command', userCommand)
     $('#terminal-cmd').val('')
+})
+
+
+$('#terminal').terminal(function (command) {
+    if (command !== '') {
+        var result = window.eval(command);
+        if (result != undefined) {
+            this.echo(String(result));
+        }
+    }
 })
