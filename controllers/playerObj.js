0f02c3f92edacf92e866d9749d74bb1216aa3216
Ben Houston <BenjaminAHouston@gmail.com>
2019-01-03 17:49:47 -0500

    Fixed a bug with effects being removed

--- controllers/playerObj.js
+++ controllers/playerObj.js
@@ -348,7 +348,7 @@ module.exports = function Player(player_id, player_name, attack, defense, hp, mp
 
     this.removeEffects = function() {
         for (let i = 0; i < this.effects.length; i++) {
-            this.effect.undoEffect(this)
+            this.effects[i].undoEffect(this)
         }
         this.effects = []
     }
@@ -362,6 +362,14 @@ module.exports = function Player(player_id, player_name, attack, defense, hp, mp
             }
             this.socket.emit('command-response', { message:  `${this.name} wakes up energized and ready!` })
             return true
+        } else {
+            if (this.room === 'class') {
+                this.socket.emit('command-response', { message: `Dan doesn't like it when you sleep during class.`, alertType: 'danger' })
+            } else if (this.room === 'panera') {
+                this.socket.emit('command-response', { message: 'One of the Panera employees suggests sleeping at home.', alertType: 'danger' })
+            } else if (this.room === 'vending machine') {
+                this.socket.emit('command-response', { message: 'How are you going to fall asleep standing up?', alertType: 'danger' })
+            }
         }
         return false
     }
